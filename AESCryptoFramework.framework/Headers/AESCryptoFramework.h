715d6a568507e5a595d1c7c4e774f57890ef475c
Runali <Runali@Runalis-MacBook-Air.local>
2024-10-05 15:31:44 +0530

    Initial Commit

---
+++ AESCryptoFramework.framework/Headers/AESCryptoFramework.h
@@ -0,0 +1,18 @@
+//
+//  AESCryptoFramework.h
+//  AESCryptoFramework
+//
+//  Created by Runali on 05/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AESCryptoFramework.
+FOUNDATION_EXPORT double AESCryptoFrameworkVersionNumber;
+
+//! Project version string for AESCryptoFramework.
+FOUNDATION_EXPORT const unsigned char AESCryptoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AESCryptoFramework/PublicHeader.h>
+
+
